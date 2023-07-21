0947b75a7e8af59691e47bdd7149c283a4c05720
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-07-21 12:39:25 +0300

    category not working

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,18 @@
 import React from "react";
+function CategoryFilter({ CATEGORIES, handleCategoryClick }) {
+  function renderCategory() {
+    return CATEGORIES.map((category) => (
+      <button key={category} onClick={() => handleCategoryClick(category)}>
+        {category}
+      </button>
+    ));
+  }
 
-function CategoryFilter() {
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {renderCategory()}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, {useState } from "react";
+import Task from "./Task";
+import CategoryFilter from "./CategoryFilter";
+import { CATEGORIES } from "../data";
 
-function TaskList() {
+function TaskList({ TASKS }) {
+  const [deleted, setDeleted] = useState(TASKS);
+  const [filter, setFilter] = useState("ALL");
+
+  function handleCategoryClick(category) {
+    if (category === "ALL") {
+      setDeleted(TASKS);
+    } else {
+      const filteredTasks = TASKS.filter((task) => task.category === category);
+      setDeleted(filteredTasks);
+    }
+    setFilter(category);
+  }
+  function handleDelete(taskText) {
+    const updatedList = deleted.filter((task) => task.text !== taskText);
+    setDeleted(updatedList);
+  }
+
+  function renderList() {
+    return deleted.map((task) => (
+      <li key={task.text}>
+        <Task task={task} handleDelete={handleDelete} />
+      </li>
+    ));
+  }
+
   return (
     <div className="tasks">
+      <CategoryFilter
+        CATEGORIES={CATEGORIES}
+        handleCategoryClick={handleCategoryClick}
+        activeCategory={filter}
+      />
       {/* display a list of tasks using Task component */}
+      <ul>{renderList()}</ul>
+      
     </div>
   );
 }
